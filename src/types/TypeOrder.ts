f271c670696aed568c7c635cfc59d931c89f6137
김명구 <myeonggu.kim@kakao.com>
2024-05-03 01:15:15 +0900

    .

---
+++ src/types/TypeOrder.ts
@@ -0,0 +1,18 @@
+import { GridColDef } from '@mui/x-data-grid';
+
+
+
+export type TypeProduct = {
+  "id": number,
+  "name": string
+}
+
+export type TypeProductItem = {
+  "id"?: number,
+  "name": string,
+  "quantity"?: number
+}
+
+export type TypeOrder = {
+  "id": number
+}
