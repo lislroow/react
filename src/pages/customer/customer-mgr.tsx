376f2d0d84e2ab45187d0095e1c98a3b5d3c1b03
김명구 <myeonggu.kim@kakao.com>
2024-05-14 21:39:51 +0900

    레이블 추가

--- src/pages/customer/customer-mgr.tsx
+++ src/pages/customer/customer-mgr.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import Grid from '@mui/material/Grid';
-import { Box, Button, TextField, FormControl, Checkbox, FormControlLabel } from '@mui/material';
+import { Box, Button, TextField, FormControl, Checkbox, FormControlLabel, Typography } from '@mui/material';
 
 import storeAlert, { actAlertShow } from 'redux-store/store-alert';
 import storeUser, { actUpdate } from 'redux-store/store-user';
@@ -125,135 +125,122 @@ const Page = () => {
         noValidate
         autoComplete="off">
         <div>
-          <Grid container spacing={1} justifyContent='flex-end' alignItems='center'>
-            <Grid item xs={12} sm={4} md={3} lg={2} style={{textAlign: 'right'}}>
-              <Button onClick={(e) => handleSaveBasic()} variant="contained">기본정보 저장</Button>
-            </Grid>
-          </Grid>
           <Grid container spacing={1} alignItems='center'>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                margin="normal"
-                InputProps={{
-                  readOnly: true,
-                }}
-                InputLabelProps={{ shrink: true }}
-                variant="standard"
-                fullWidth
-                id="id"
-                name="id"
-                label="id"
-                value={customer.id}
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                margin="normal"
-                InputProps={{
-                  readOnly: true,
-                }}
-                InputLabelProps={{ shrink: true }}
-                variant="standard"
-                fullWidth
-                id="email"
-                name="email"
-                label="email"
-                value={customer.email}
-              />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                margin="normal"
-                InputLabelProps={{ shrink: true }}
-                variant="standard"
-                fullWidth
-                id="name"
-                name="name"
-                label="name"
-                value={customer.name}
-                onChange={(event) => setCustomer({ ...customer, name: event.target.value })}
-              />
-            </Grid>
-          </Grid>
-          
-          {/* <Grid container spacing={1} alignItems='center'>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                margin="normal"
-                InputLabelProps={{ shrink: true }}
-                variant="standard"
-                fullWidth
-                id="password"
-                name="password"
-                label="password"
-                value={customer.password}
-                type="password"
-              />
-            </Grid>
-          </Grid>
-          <Grid container spacing={1} alignItems='center'>
-            <Grid item xs={12} sm={4}>
-              <TextField
-                margin="normal"
-                InputLabelProps={{ shrink: true }}
-                variant="standard"
-                fullWidth
-                id="password_confirm"
-                name="password_confirm"
-                label="password confirm"
-                value={customer.password_confirm}
-                type="password"
-              />
-            </Grid>
-          </Grid> */}
-          
-          <Grid container spacing={1} justifyContent='flex-end' alignItems='center' direction='row' style={{marginTop: '8px'}}>
-            <Grid item xs={12} sm={4} md={3} lg={2} style={{textAlign: 'right'}}>
-              <Button onClick={(e) => handleAddDelivery()} variant="contained" style={{marginRight: '8px'}}>추가</Button>
-              <Button onClick={(e) => handleSaveDeliveryList()} variant="contained">저장</Button>
-            </Grid>
-          </Grid>
-          {
-            deliveryList.map((delivery, index) => (
-              <Grid container key={index} spacing={1} alignItems='center'>
+            <Grid item xs={12}>
+              <Grid container alignItems='center'>
                 <Grid item xs={12}>
-                  <Grid container spacing={1} alignItems='center'>
-                    <Grid item>
-                      <FormControl>
-                        <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={delivery.primaryYn === 'Y'}
-                              onChange={handleCheckboxChange(index)}
-                              color="primary"
-                            />
-                          }
-                          label=""
-                        />
-                      </FormControl>
-                    </Grid>
-                    <Grid item>
-                      <TextField
-                        margin="normal"
-                        InputLabelProps={{ shrink: true }}
-                        variant="standard"
-                        fullWidth
-                        id={`delivery_${index}`}
-                        name={`delivery_${index}`}
-                        label={delivery.primaryYn === 'Y' ? '기본 배송지' : '배송지'}
-                        value={delivery.address}
-                        onChange={(e) => {
-                          const updatedList = [...deliveryList];
-                          updatedList[index].address = e.target.value;
-                          setDeliveryList(updatedList);
-                        }}
-                      />
-                    </Grid>
+                  <Typography>- 기본정보</Typography>
+                </Grid>
+              </Grid>
+              <Grid container alignItems='center'>
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    margin="normal"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    InputLabelProps={{ shrink: true }}
+                    variant="standard"
+                    fullWidth
+                    id="id"
+                    name="id"
+                    label="id"
+                    value={customer.id}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    margin="normal"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    InputLabelProps={{ shrink: true }}
+                    variant="standard"
+                    fullWidth
+                    id="email"
+                    name="email"
+                    label="email"
+                    value={customer.email}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    margin="normal"
+                    InputLabelProps={{ shrink: true }}
+                    variant="standard"
+                    fullWidth
+                    id="name"
+                    name="name"
+                    label="name"
+                    value={customer.name}
+                    onChange={(event) => setCustomer({ ...customer, name: event.target.value })}
+                  />
+                </Grid>
+              </Grid>
+              <Grid item xs={12}>
+                <Grid container spacing={1} justifyContent='flex-end' alignItems='center'>
+                  <Grid item xs={12} sm={4} md={3} lg={2} style={{textAlign: 'right'}}>
+                    <Button onClick={(e) => handleSaveBasic()} variant="contained">기본정보 저장</Button>
                   </Grid>
                 </Grid>
               </Grid>
-            ))
-          }
+            </Grid>
+            
+            <Grid item xs={12}>
+              <Grid container alignItems='center'>
+                <Grid item xs={12}>
+                  <Typography>- 배송지</Typography>
+                </Grid>
+              </Grid>
+              {
+                deliveryList.map((delivery, index) => (
+                  <Grid container key={index} spacing={1} alignItems='center'>
+                    <Grid item xs={12}>
+                      <Grid container spacing={1} alignItems='center'>
+                        <Grid item>
+                          <FormControl>
+                            <FormControlLabel
+                              control={
+                                <Checkbox
+                                  checked={delivery.primaryYn === 'Y'}
+                                  onChange={handleCheckboxChange(index)}
+                                  color="primary"
+                                />
+                              }
+                              label=""
+                            />
+                          </FormControl>
+                        </Grid>
+                        <Grid item>
+                          <TextField
+                            margin="normal"
+                            InputLabelProps={{ shrink: true }}
+                            variant="standard"
+                            fullWidth
+                            id={`delivery_${index}`}
+                            name={`delivery_${index}`}
+                            label={delivery.primaryYn === 'Y' ? '기본 배송지' : '배송지'}
+                            value={delivery.address}
+                            onChange={(e) => {
+                              const updatedList = [...deliveryList];
+                              updatedList[index].address = e.target.value;
+                              setDeliveryList(updatedList);
+                            }}
+                          />
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                ))
+              }
+              <Grid container justifyContent='flex-end' alignItems='center' direction='row' style={{marginTop: '8px'}}>
+                <Grid item xs={12} sm={4} md={3} lg={2} style={{textAlign: 'right'}}>
+                  <Button onClick={(e) => handleAddDelivery()} variant="contained" style={{marginRight: '8px'}}>추가</Button>
+                  <Button onClick={(e) => handleSaveDeliveryList()} variant="contained">저장</Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
         </div>
       </Box>
     </Layout>
