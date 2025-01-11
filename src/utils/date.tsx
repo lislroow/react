40d1b0ab39bc338ddb239377c04e5bf1d9901aab
김명구 <myeonggu.kim@kakao.com>
2025-01-12 00:06:08 +0900

    clean: date.tsx

--- src/utils/date.tsx
+++
@@ -1,7 +0,0 @@
-
-export const formatDate = function(date: string): string {
-  if (date) {
-    new Intl.DateTimeFormat('ko-KR', { dateStyle: 'medium' }).format(new Date(date));
-  }
-  return '';
-} 
